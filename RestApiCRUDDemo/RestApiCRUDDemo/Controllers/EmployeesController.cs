04d58100278bd05f8eda2deae0f0bea6ce35b4cf
unknown <aydinterzi7@gmail.com>
2021-08-02 09:58:42 +0300

    first commit

---
+++ RestApiCRUDDemo/RestApiCRUDDemo/Controllers/EmployeesController.cs
@@ -0,0 +1,116 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using RestApiCRUDDemo.EmployeeData;
+using RestApiCRUDDemo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestApiCRUDDemo.Controllers
+{
+
+    [ApiController]
+    public class EmployeesController : ControllerBase
+    {
+        private readonly ILogger<EmployeesController> _logger;
+        private IEmployeeData _employeeData;
+        public EmployeesController(IEmployeeData employeeData,ILogger<EmployeesController> logger)
+        {
+            _logger = logger;
+            _employeeData = employeeData;
+        }
+
+        [HttpGet]
+        [Route("api/[controller]")]
+        public IActionResult GetEmployees()
+        {
+            try
+            {
+                _logger.LogInformation("Fetching all the employees from the storage");
+                return Ok(_employeeData.GetEmployees());
+            }
+            catch (Exception ex)
+            {
+
+                _logger.LogError("something went wrong:");
+                return StatusCode(500, "internal server error");
+            }
+            
+        }
+        [HttpGet]
+        [Route("api/[controller]/{id}")]
+        public IActionResult GetEmployee(Guid id)
+        {
+            var employee = _employeeData.GetEmployee(id);
+            if (employee != null)
+            {
+                return Ok(_employeeData.GetEmployee(id));
+            }
+            return NotFound($"Employee with Id:{id} was not found");
+        }
+        [HttpPost]
+        [Route("api/[controller]")]
+        public IActionResult PostEmployee(Employee employee)
+        {
+            try
+            {
+                _logger.LogInformation("Post is success");
+                _employeeData.AddEmployee(employee);
+                return Created(HttpContext.Request.Scheme + "://" + HttpContext.Request.Host + HttpContext.Request.Path + "/" + employee.Id, employee);
+
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError("something went wrong");
+                return StatusCode(500);
+            }
+        }
+
+        [HttpDelete]
+        [Route("api/[controller]/{id}")]
+        public IActionResult DeleteEmployee(Guid id)
+        {
+            try
+            {
+                var employee = _employeeData.GetEmployee(id);
+                if (employee != null)
+                {
+                    _logger.LogInformation("Employee deleted.");
+                    _employeeData.DeleteEmployee(employee);
+                    return Ok();
+                }
+                return NotFound($"Employee with Id:{id} was not found");
+            }
+            catch (Exception)
+            {
+                _logger.LogError("something went wrong");
+                return StatusCode(500);
+            }
+            
+        }
+        [HttpPatch]
+        [Route("api/[controller]/{id}")]
+        public IActionResult EditEmployee(Guid id, Employee employee)
+        {
+            try
+            {
+                var existingEmployee = _employeeData.GetEmployee(id);
+                if (existingEmployee != null)
+                {
+                    _logger.LogInformation("");
+                    employee.Id = existingEmployee.Id;
+                    _employeeData.EditEmployee(employee);
+                }
+                return Ok(employee);
+            }
+            catch (Exception)
+            {
+                _logger.LogError("something went wrong");
+                return StatusCode(500);
+            }
+            
+        }
+    }
+}
---
+++ RestApiCRUDDemo/RestApiCRUDDemo/EmployeeData/SqlEmployeeData.cs
@@ -0,0 +1,54 @@
+﻿using RestApiCRUDDemo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestApiCRUDDemo.EmployeeData
+{
+    public class SqlEmployeeData : IEmployeeData
+    {
+        private EmployeeContext _employeeContext;
+        public SqlEmployeeData(EmployeeContext employeeContext)
+        {
+            _employeeContext = employeeContext;
+        }
+        public Employee AddEmployee(Employee employee)
+        {
+            employee.Id = Guid.NewGuid();
+            _employeeContext.Employees.Add(employee);
+            _employeeContext.SaveChanges();
+            return employee;
+        }
+
+        public void DeleteEmployee(Employee employee)
+        {
+            _employeeContext.Employees.Remove(employee);
+            _employeeContext.SaveChanges();
+
+        }
+
+        public Employee EditEmployee(Employee employee)
+        {
+            var existingEmployee = _employeeContext.Employees.Find(employee.Id);
+            if (existingEmployee != null)
+            {
+                existingEmployee.Name = employee.Name;
+                _employeeContext.Employees.Update(existingEmployee);
+                _employeeContext.SaveChanges();
+            }
+            return employee;
+        }
+
+        public Employee GetEmployee(Guid id)
+        {
+            var employee = _employeeContext.Employees.Find(id);
+            return employee;
+        }
+
+        public List<Employee> GetEmployees()
+        {
+           return _employeeContext.Employees.ToList();
+        }
+    }
+}
